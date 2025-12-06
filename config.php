31b9f692b13c9ade67b8065792a79314194e48df
joseluisth01 <joseluistirado02@gmail.com>
2025-12-06 22:40:51 +0100

    ergserg

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'hype-login');
+define('DB_NAME', 'hype_login');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
